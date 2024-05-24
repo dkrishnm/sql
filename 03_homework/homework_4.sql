0764e6e5dbb25200b5419ba1512e031ef79b4fdd
dkrishnm <div.k.moorthy@gmail.com>
2024-05-23 23:34:23 -0400

    Updates for homework4

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -37,7 +37,7 @@ HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 SELECT 
     customer_id,
     market_date,
-    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date) AS visit_number
+    DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY market_date) AS visit_number
 FROM 
     customer_purchases;
 
@@ -50,7 +50,7 @@ with ordered_visits as (
 SELECT 
     customer_id,
     market_date,
-    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date desc) AS visit_number
+    DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY market_date desc) AS visit_number
 FROM 
     customer_purchases ) 	
 select customer_id, market_date, visit_number  from ordered_visits where visit_number = 1
@@ -59,7 +59,9 @@ select customer_id, market_date, visit_number  from ordered_visits where visit_n
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
 
-
+SELECT customer_id, product_id, COUNT(*) AS purchase_count
+FROM customer_purchases
+GROUP BY customer_id, product_id;
 
 
 -- String manipulations
@@ -74,11 +76,26 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
-
+SELECT product_id,
+       product_name,
+       CASE WHEN INSTR(product_name, '-') > 0
+            THEN TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1))
+       ELSE NULL
+       END AS description
+FROM product;
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
 
-
+SELECT
+    product_id,
+    product_name,
+    product_size,
+    product_category_id,
+    product_qty_type,
+    TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1)) AS description
+FROM product
+WHERE
+	product_size REGEXP '[0-9]';
 
 -- UNION
 /* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
@@ -94,9 +111,7 @@ with a UNION binding them. */
 
 
 
-SELECT customer_id, product_id, COUNT(*) AS purchase_count
-FROM customer_purchases
-GROUP BY customer_id, product_id;
+
 
 
 
